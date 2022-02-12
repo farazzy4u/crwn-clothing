771fc8005ecc5a8162fc0b8ee8648f91d9a7d8c7
faraz <farazzy4u@gmail.com>
2022-02-11 20:53:37 -0600

    - added collection-item - shop data js - collection-preview

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <switch>
+      <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
-      </switch>
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
